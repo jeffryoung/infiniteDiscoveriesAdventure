ded0ce7e579c50980a27edac3f17786e3e4e6ce1
Jeffrey Young <jyoung@idiscoveries.org>
2015-10-29 13:37:17 -0700

    Initial Commit

---
+++ infiniteDiscoveriesAdventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  infiniteDiscoveriesAdventure
+//
+//  Created by Jeffrey Young on 10/29/15.
+//  Copyright © 2015 infinite Discoveries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ infiniteDiscoveriesAdventure/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  infiniteDiscoveriesAdventure
+//
+
+//  Copyright (c) 2015 infinite Discoveries. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
